fbf51850e480ca0e42b54f562f05bc6e78341f89
Mukhtarrahimi <mukhtarrahimi.dev@gmail.com>
2026-01-03 09:33:06 +0430

    backend: db.js code update

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async() => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error(`Error: ${error.message}`);
+        console.error("MongoDB connection error:", error.message);
         process.exit(1);
     }
 };

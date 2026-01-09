6d26980c6175869d72be76e5c0b48d1013016ed4
Mukhtarrahimi <mukhtarrahimi.dev@gmail.com>
2026-01-09 14:41:51 +0430

    backend: logout controller

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -3,115 +3,131 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { generateAccessToken, generateRefreshToken } = require("../utils/token");
 const {
-    registerValidation,
-    loginValidation,
+  registerValidation,
+  loginValidation,
 } = require("../validations/authValidation");
 
-exports.register = async(req, res) => {
-    try {
-        const { error, value } = registerValidation(req.body);
-        const { name, username, email, password, phone } = value;
+exports.register = async (req, res) => {
+  try {
+    const { error, value } = registerValidation(req.body);
+    const { name, username, email, password, phone } = value;
 
-        if (error) {
-            return res.status(400).json({
-                success: false,
-                message: "Validation failed",
-                details: error.details.map((detail) => detail.message),
-            });
-        }
-
-        const existingUser = await User.findOne({
-            $or: [{ email }, { username }, { phone }],
-        });
-
-        if (existingUser) {
-            return res.status(400).json({
-                success: false,
-                message: "User already exists",
-            });
-        }
-
-        const user = await User.create({
-            name,
-            username,
-            email,
-            phone,
-            hashPassword: password,
-        });
-
-        const accessToken = generateAccessToken(user._id);
-        const refreshToken = generateRefreshToken(user._id);
-
-        user.refreshToken = refreshToken;
-        await user.save();
-
-        res.status(201).json({
-            success: true,
-            user: user.toJSON(),
-            accessToken,
-            refreshToken,
-        });
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: "Server error",
-            error: err.message,
-        });
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        message: "Validation failed",
+        details: error.details.map((detail) => detail.message),
+      });
     }
+
+    const existingUser = await User.findOne({
+      $or: [{ email }, { username }, { phone }],
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const user = await User.create({
+      name,
+      username,
+      email,
+      phone,
+      hashPassword: password,
+    });
+
+    const accessToken = generateAccessToken(user._id);
+    const refreshToken = generateRefreshToken(user._id);
+
+    user.refreshToken = refreshToken;
+    await user.save();
+
+    res.status(201).json({
+      success: true,
+      user: user.toJSON(),
+      accessToken,
+      refreshToken,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: err.message,
+    });
+  }
 };
 
 // Login controller
-exports.login = async(req, res) => {
-    try {
-        const { error, value } = loginValidation(req.body);
-        if (error) {
-            return res.status(400).json({
-                success: false,
-                message: "validation failed",
-                details: error.details.map((detail) => detail.message),
-            });
-        }
-        const { email, password } = value;
-        const user = await User.findOne({ email }).select(
-            "+hashPassword +refreshToken"
-        );
-        if (!user) {
-            return res.status(401).json({
-                success: false,
-                message: "Invalid credentials",
-            });
-        }
-
-        const isMatch = await bcrypt.compare(password, user.hashPassword);
-        if (!isMatch) {
-            return res.status(400).json({
-                success: false,
-                message: "Invalid credentials",
-            });
-        }
-
-        // generate tokens
-        const accessToken = generateAccessToken(user._id);
-        const refreshToken = generateRefreshToken(user._id);
-
-        user.refreshToken = refreshToken;
-        await user.save();
-
-        res.status(200).json({
-            success: true,
-            user: user.toJSON(),
-            accessToken,
-            refreshToken,
-        });
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: "Server error",
-            error: err.message,
-        });
+exports.login = async (req, res) => {
+  try {
+    const { error, value } = loginValidation(req.body);
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        message: "validation failed",
+        details: error.details.map((detail) => detail.message),
+      });
     }
+    const { email, password } = value;
+    const user = await User.findOne({ email }).select(
+      "+hashPassword +refreshToken"
+    );
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.hashPassword);
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    // generate tokens
+    const accessToken = generateAccessToken(user._id);
+    const refreshToken = generateRefreshToken(user._id);
+
+    user.refreshToken = refreshToken;
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      user: user.toJSON(),
+      accessToken,
+      refreshToken,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: err.message,
+    });
+  }
 };
 
+// logout controller
+exports.logout = async (req, res) => {
+  try {
+    const userId = req.user._id;
 
+    await User.findByIdAndUpdate(userId, { $unset: { refreshToken: 1 } });
 
-// forgot password controller
+    res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "server error",
+      error: err.message,
+    });
+  }
+};

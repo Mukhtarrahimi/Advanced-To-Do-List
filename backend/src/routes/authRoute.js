0bea2edaf7d9a150053d3deabf891daaad3571dd
Mukhtarrahimi <mukhtarrahimi.dev@gmail.com>
2026-01-10 09:47:33 +0430

    backend: gitMe route

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -3,10 +3,11 @@ const router = express.Router();
 // imorted
 const
     protect = require("../middlewares/authMiddleware");
-const { register, login, logout } = require("../controllers/authController");
+const { register, login, logout, getMe } = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", protect, logout);
+router.get('/me', protect, getMe)
 
 module.exports = router;

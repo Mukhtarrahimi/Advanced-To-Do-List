24829efebf7042c0e6113d8a04654bfc7d13eccc
Mukhtarrahimi <mukhtarrahimi.dev@gmail.com>
2026-01-10 10:21:49 +0430

    backend: update profile route

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
 // imorted
-const
-    protect = require("../middlewares/authMiddleware");
-const { register, login, logout, getMe } = require("../controllers/authController");
+const protect = require("../middlewares/authMiddleware");
+const {
+  register,
+  login,
+  logout,
+  getMe,
+  updateProfile,
+} = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", protect, logout);
-router.get('/me', protect, getMe)
+router.get("/me", protect, getMe);
+router.put("/update-profile", protect, updateProfile);
 
 module.exports = router;

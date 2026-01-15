2d2cf4c6b9117aa6eb487ab50f03f1a4f452fc14
Mukhtarrahimi <mukhtarrahimi.dev@gmail.com>
2026-01-15 14:45:23 +0430

    backend: logout add to authRoute

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { register, login } = require("../controllers/authController");
 const router = express.Router();
+// imorted
+const { protect } = require("../middlewares/authMiddleware");
+const { register, login, logout } = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", protect, logout);
 
 module.exports = router;

1339535a2839c2a556ee601c62da8cd9d4a9e3ff
Mukhtarrahimi <mukhtarrahimi.dev@gmail.com>
2026-01-17 13:50:36 +0430

    frontend: dashboard/page.js

---
+++ frontend/src/app/dashboard/page.jsx
@@ -0,0 +1,8 @@
+export default function DashboardHome() {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold"> Dashboard </h1>{" "}
+      <p> Welcome to your Todo Dashboard! </p>{" "}
+    </div>
+  );
+}
